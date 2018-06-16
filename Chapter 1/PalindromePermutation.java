d288f43c3b3e6eb7a05fa1cd39be4b80f035e59a
Aaron Bargotta <bargotta@princeton.edu>
2018-06-16 00:28:47 -0400

    1.4 Palindrome Permutation

--- Chapter 1/PalindromePermutation.java
+++ Chapter 1/PalindromePermutation.java
@@ -5,7 +5,6 @@
  * a palindrome. A palindrome is a word or phrase that is the same forwards
  * and backwards. A permutation is a rearrangement of letters. The palindrome
  * does not need to be limited to just dictionary words.
- *
  * EXAMPLE
  * Input:  Tact Coa
  * Output: True (permutations: "taco cat", "atcocta", etc.)
